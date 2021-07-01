ae1f1a15e6df616bdef10933832f0b0c39500ee9
Szymon R <szymon@rajewski.co.uk>
2021-07-01 16:37:41 +0200

    Adres heroku

--- static/function_graph.js
+++ static/function_graph.js
@@ -1,5 +1,5 @@
 const DEV_API_URL = 'http://127.0.0.1:5000/api/calculate?function=';
-const API_URL = '';
+const API_URL = 'https://hudson-toque-06982.herokuapp.com/api/calculate?function=';
 
 async function calculateFunction(formula) {
     if (formula) {
@@ -11,7 +11,7 @@ async function calculateFunction(formula) {
 }
 
 async function postFunction(func) {
-    const response = await fetch(DEV_API_URL + encodeURI(func), {
+    const response = await fetch(API_URL + encodeURI(func), {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
